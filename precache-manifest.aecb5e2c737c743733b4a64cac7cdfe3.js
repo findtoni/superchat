9292146846003a79ad28e3751df3383735d31da5
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-10-05 19:31:21 +0100

    Updates

---
+++ precache-manifest.aecb5e2c737c743733b4a64cac7cdfe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03da1a6d2ff48c5185ab53277bfb8f67",
+    "url": "/superchat/index.html"
+  },
+  {
+    "revision": "06d63dc8fc4fbc6a9202",
+    "url": "/superchat/static/css/main.979ca5c4.chunk.css"
+  },
+  {
+    "revision": "647469ef53970b9bb0fb",
+    "url": "/superchat/static/js/2.bbe4bffd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/superchat/static/js/2.bbe4bffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d63dc8fc4fbc6a9202",
+    "url": "/superchat/static/js/main.ee024802.chunk.js"
+  },
+  {
+    "revision": "a8db42cece2ba32e15a0",
+    "url": "/superchat/static/js/runtime-main.eecd1cc6.js"
+  }
+]);
