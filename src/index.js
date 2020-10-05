59fecf6bb5cde587e5a62831b151ceeb004a19c4
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-10-05 19:19:12 +0100

    c'este fini

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './App/App';
+import './assets/main.css'
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+function dark(state = false, action) {
+  switch (action.type) {
+    case 'ISDARK':
+      return true
+    case 'ISLIGHT':
+      return false
+    default:
+      return state
+  }
+}
+
+const store = createStore(dark);
+
+const render = () => ReactDOM.render(
+  <React.StrictMode store={store}>
+    <App
+      dark={store.getState()}
+      isDark={() => store.dispatch({ type: 'ISDARK' })}
+      isLight={() => store.dispatch({ type: 'ISLIGHT' })}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+render()
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
