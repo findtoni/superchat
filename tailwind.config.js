df49ec9aa0cc3493452da4b8fa7e4ba127b3d7b7
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-10-05 19:16:45 +0100

    added tailwind & postcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    container: {
+			center: true,
+      padding: {
+        default: '2rem',
+        sm: '2rem',
+        lg: '1rem',
+        xl: '6rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
